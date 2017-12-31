0a9c0f79873eab39c01dadf2fcd7163d5025c99d
rhysd <lin90162@yahoo.co.jp>
2017-12-31 13:35:22 +0900

    add tests for .tar.xz and .xz releases

--- selfupdate/uncompress_test.go
+++ selfupdate/uncompress_test.go
@@ -27,6 +27,16 @@ func TestCompressionNotRequired(t *testing.T) {
 	}
 }
 
+func getArchiveFileExt(file string) string {
+	if strings.HasSuffix(file, ".tar.gz") {
+		return ".tar.gz"
+	}
+	if strings.HasSuffix(file, ".tar.xz") {
+		return ".tar.xz"
+	}
+	return filepath.Ext(file)
+}
+
 func TestUncompress(t *testing.T) {
 	for _, n := range []string{
 		"testdata/foo.zip",
@@ -34,6 +44,8 @@ func TestUncompress(t *testing.T) {
 		"testdata/single-file.gz",
 		"testdata/single-file.gzip",
 		"testdata/foo.tar.gz",
+		"testdata/foo.tar.xz",
+		"testdata/single-file.xz",
 	} {
 		t.Run(n, func(t *testing.T) {
 			f, err := os.Open(n)
@@ -41,13 +53,7 @@ func TestUncompress(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			var ext string
-			if strings.HasSuffix(n, ".tar.gz") {
-				ext = ".tar.gz"
-			} else {
-				ext = filepath.Ext(n)
-			}
-
+			ext := getArchiveFileExt(n)
 			url := "https://github.com/foo/bar/releases/download/v1.2.3/bar" + ext
 			r, err := UncompressCommand(f, url, "bar")
 			if err != nil {
@@ -75,19 +81,16 @@ func TestUncompressInvalidArchive(t *testing.T) {
 		{"testdata/invalid.gz", "Failed to uncompress gzip file"},
 		{"testdata/invalid-tar.tar.gz", "Failed to unarchive .tar file"},
 		{"testdata/invalid-gzip.tar.gz", "Failed to uncompress .tar.gz file"},
+		{"testdata/invalid.xz", "Failed to uncompress xzip file"},
+		{"testdata/invalid-tar.tar.xz", "Failed to unarchive .tar file"},
+		{"testdata/invalid-xz.tar.xz", "Failed to uncompress .tar.xz file"},
 	} {
 		f, err := os.Open(a.name)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		var ext string
-		if strings.HasSuffix(a.name, ".tar.gz") {
-			ext = ".tar.gz"
-		} else {
-			ext = filepath.Ext(a.name)
-		}
-
+		ext := getArchiveFileExt(a.name)
 		url := "https://github.com/foo/bar/releases/download/v1.2.3/bar" + ext
 		_, err = UncompressCommand(f, url, "bar")
 		if err == nil {
@@ -99,59 +102,29 @@ func TestUncompressInvalidArchive(t *testing.T) {
 	}
 }
 
-func TestTargetNotFoundInZip(t *testing.T) {
-	for _, f := range []string{
-		"testdata/empty.zip",
-		"testdata/bar-not-found.zip",
+func TestTargetNotFound(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		msg  string
+	}{
+		{"testdata/empty.zip", "command is not found"},
+		{"testdata/bar-not-found.zip", "command is not found"},
+		{"testdata/bar-not-found.gzip", "does not match to command"},
+		{"testdata/empty.tar.gz", "command is not found"},
+		{"testdata/bar-not-found.tar.gz", "command is not found"},
 	} {
-		t.Run(f, func(t *testing.T) {
-			f, err := os.Open(f)
+		t.Run(tc.name, func(t *testing.T) {
+			f, err := os.Open(tc.name)
 			if err != nil {
 				t.Fatal(err)
 			}
-
-			_, err = UncompressCommand(f, "https://github.com/foo/bar/releases/download/v1.2.3/bar.zip", "bar")
+			ext := getArchiveFileExt(tc.name)
+			url := "https://github.com/foo/bar/releases/download/v1.2.3/bar" + ext
+			_, err = UncompressCommand(f, url, "bar")
 			if err == nil {
 				t.Fatal("Error should be raised for")
 			}
-			if !strings.Contains(err.Error(), "command is not found") {
-				t.Fatal("Unexpected error:", err)
-			}
-		})
-	}
-}
-
-func TestTargetNotFoundInGZip(t *testing.T) {
-	f, err := os.Open("testdata/bar-not-found.gzip")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = UncompressCommand(f, "https://github.com/foo/bar/releases/download/v1.2.3/bar.gzip", "bar")
-	if err == nil {
-		t.Fatal("Error should be raised for")
-	}
-	if !strings.Contains(err.Error(), "does not match to command") {
-		t.Fatal("Unexpected error:", err)
-	}
-}
-
-func TestTargetNotFoundInTarGz(t *testing.T) {
-	for _, f := range []string{
-		"testdata/empty.tar.gz",
-		"testdata/bar-not-found.tar.gz",
-	} {
-		t.Run(f, func(t *testing.T) {
-			f, err := os.Open(f)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			_, err = UncompressCommand(f, "https://github.com/foo/bar/releases/download/v1.2.3/bar.tar.gz", "bar")
-			if err == nil {
-				t.Fatal("Error should be raised for")
-			}
-			if !strings.Contains(err.Error(), "command is not found") {
+			if !strings.Contains(err.Error(), tc.msg) {
 				t.Fatal("Unexpected error:", err)
 			}
 		})
